99c4ce84887b3233ace17130d06ab89bb2d88278
eperezvazquez <perezelisa21@gmail.com>
2022-08-02 17:09:47 +0000

    Kmean

--- src/app.py
+++ src/app.py
@@ -1,4 +1,84 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import folium
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.vq import kmeans, vq
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
-# your code here
+url='https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv'
+df_raw=pd.read_csv(url)
+df_raw
+
+df=df_raw[['MedInc','Latitude','Longitude']]
+df
+
+escalador=StandardScaler()
+df_norm=escalador.fit_transform(df)
+df_norm
+
+#cluster con mas cohesion -->metodo del codo y coef de silueta
+#METODO DEL CODO
+sum_distancias_cuadradas = []
+#inercia calcula las distancias cuadradas
+
+K = range(1,10)
+
+for num_clusters in K :
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(df_norm)
+    sum_distancias_cuadradas.append(kmeans.inertia_)
+plt.plot(K,sum_distancias_cuadradas,'bx-')
+plt.xlabel('Cantidad de clusters (k)')
+plt.ylabel('Suma de distancias cuadradas (Inercia)')
+plt.title('Elbow Method para determinar k óptimo')
+plt.show()
+
+rango_n_clusters = [2, 3, 4, 5, 6, 7, 8,9,10]
+silhouette_avg = []
+for num_clusters in rango_n_clusters:
+# fit Kmeans
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(df_norm)
+    cluster_labels = kmeans.labels_
+# calcular silhouette
+    silhouette_avg.append(silhouette_score(df_norm, cluster_labels))
+plt.plot(rango_n_clusters,silhouette_avg,'bx-')
+plt.xlabel('Cantidad de cluster (k)')
+plt.ylabel('Silhouette score')
+plt.title('Silhouette para determinar k óptimo')
+plt.show()
+
+#nos quedamos con k=2 porque es es el score de silueta dio mas alto.
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(df_norm)
+
+df2=escalador.inverse_transform(df_norm)
+
+df2=pd.DataFrame(df2,columns=['MedInc','Latitude','Longitude','Cluster'])
+
+df2['Cluster'] = kmeans.labels_
+df2
+
+sns.relplot(x='Latitude', y='Longitude', data=df2, kind='scatter', size = 'MedInc',hue='Cluster')
+plt.show()
+
+######## grafico 3D ##########
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = np.array(df2['Latitude'])
+y = np.array(df2['Longitude'])
+z = np.array(df2['MedInc'])
+
+ax.scatter(x,y,z, c=df2["Cluster"], s=40)
+
+plt.show()
+
+
